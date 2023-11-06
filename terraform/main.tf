ef0f26d131b454f8a6c4ece786c3162642f77d2e
Marcos <marcos-souza@outlook.com>
2023-11-06 06:50:52 -0300

    Correção do arquivo main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,14 +33,14 @@ resource "aws_security_group" "qtop_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
