f9c6c9c75ca8e2dfdb93f45da85d8a9b2d0d9158
Marcos <marcos-souza@outlook.com>
2023-11-06 11:12:25 -0300

    Atualização de vários arquivos

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "access_key" {
+  type = "string"
+  description = "Chave de acesso"
+  default = "AKIA6LLYOT7WVRFWO6MR"
+}
+
+variable "secret_key" {
+  type = "string"
+  description = "Senha de acesso"
+  default = "7ORxfRxajPR+8wtzBVcr02OpIa2SJYZneh5/WGp/"
+}
+
+variable "region" {
+  type = "string"
+  description = "Região padrão de provisionamento"
+  default = "us-east-1"
+}
