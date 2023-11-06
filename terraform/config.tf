a1b6229701e23b64e076325028c1108c45fd3346
Marcos <marcos-souza@outlook.com>
2023-11-06 11:26:06 -0300

    Correções de conexão

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     region = "us-east-1"
-    access_key = "AKIA6LLYOT7WVRFWO6MR"
-    secret_key = "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"
+    access_key = var.access_key
+    secret_key = var.secret_key
 }
