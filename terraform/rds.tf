7e12d584fac9a973a16bc7972d4cbcc650401395
Marcos <marcos-souza@outlook.com>
2023-11-07 14:01:20 -0300

    Implementação do aws_db_proxy e root user

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -14,6 +14,29 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
 }
 
+# Crie um proxy para conexão ao banco de dados
+resource "aws_db_proxy" "proxy" {
+  name                   = "proxy"
+  debug_logging          = false
+  engine_family          = "MYSQL"
+  idle_client_timeout    = 1800
+  require_tls            = true
+  role_arn               = "arn:aws:iam::${var.id}:role/${var.role}"
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  vpc_subnet_ids         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+
+  auth {
+    client_password_auth_type = "MYSQL_NATIVE_PASSWORD"
+    description = "Autenticação padrão com a senha do MySQL"
+    iam_auth    = "DISABLED"
+  }
+
+  tags = {
+    Name = "proxy"
+    Key  = "connect"
+  }
+}
+
 # Crie um banco de dados e tabelas na instância RDS MySQL
 resource "null_resource" "create_database_and_tables" {
   depends_on = [aws_db_instance.default]
