f33ab9a8fe494c83daad6c95eefaf7cce466cc09
Marcos <marcos-souza@outlook.com>
2023-11-07 17:28:09 -0300

    Configurações do IAM Authentication

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -21,7 +21,7 @@ resource "aws_db_proxy" "dbproxy" {
   engine_family          = "MYSQL"
   idle_client_timeout    = 1800
   require_tls            = true
-  role_arn               = arn:aws:iam::986484482029:role/role_to_get_access
+  role_arn               = "arn:aws:iam::${var.id}:role/${var.role}"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   vpc_subnet_ids         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 
