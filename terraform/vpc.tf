81e3d692c4bde7e1aab7a05279bc1046369db9dd
Marcos <marcos-souza@outlook.com>
2023-11-07 13:11:23 -0300

    Testando com o cidr livre

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -23,14 +23,14 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
