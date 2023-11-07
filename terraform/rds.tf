c2bad32b7d9fec11c2e59f06c45f3b3cbcecd32d
Marcos <marcos-souza@outlook.com>
2023-11-07 15:54:05 -0300

    correção do rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -13,7 +13,6 @@ resource "aws_db_instance" "default" {
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
 
-  skip_final_snapshot = true
 }
 
 # Crie um banco de dados e tabelas na instância RDS MySQL
