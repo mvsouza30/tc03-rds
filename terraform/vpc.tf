7e8c525eebb30b4337ec4fdc17bb695e9ac43b8a
Marcos <marcos-souza@outlook.com>
2023-11-07 12:12:27 -0300

    Corrigindo as regras de ingress e egress

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -16,33 +16,22 @@ resource "aws_subnet" "subnet_b" {
 
 resource "aws_security_group" "rds_sg" {
   name_prefix = "rds-"
-  description = "Allow TLS inbound traffic"
+  description = "Allow TLS inbound traffic"  
+  vpc_id = aws_vpc.my_vpc.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
   }
-
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-  }   
-
-
-  vpc_id = aws_vpc.my_vpc.id
-
-  # Add any additional ingress/egress rules as needed
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
   }
+
 }
 
 resource "aws_db_subnet_group" "my_db_subnet_group" {
