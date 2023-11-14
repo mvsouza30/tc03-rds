29ccbbe24ee934a4c46bd03ee1c232f017fbc488
Marcos <marcos-souza@outlook.com>
2023-11-14 15:10:58 -0300

    Troubleshooting na Default VPC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,13 +23,13 @@ variable "db_password" {
 variable "availability_zone_01" {
   description = "Primeira zona de disponibilidade do RDS"
   type        = string
-  default     = "us-east-1a"
+  default     = "us-east-1b"
 }
 
 variable "availability_zone_02" {
   description = "Segunda zona de disponibilidade do RDS"
   type        = string
-  default     = "us-east-1b"
+  default     = "us-east-1c"
 }
 
 variable "subnet_group_name"{
