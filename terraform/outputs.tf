372f810098c087b29fdee37c2ce52a9ef2b15e24
Marcos <marcos-souza@outlook.com>
2023-11-15 08:31:36 -0300

    Criação do outputs.tf, correções de conexao ao banco

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,17 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.rds-sbnt-grp.address
+  sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
   value       = aws_db_instance.rds-sbnt-grp.port
+  sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance port"
   value       = aws_db_instance.rds-sbnt-grp.username
+  sensitive   = true
 }
